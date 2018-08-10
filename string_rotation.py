535eaaea1368c3d5aba9b2fee6b00163ad4521f1
Sacha Gunaratne <sacha.gunaratne@gmail.com>
2018-08-09 20:18:18 -0400

    String rotation question.

---
+++ string_rotation.py
@@ -0,0 +1,22 @@
+"""Cracking the Coding Interview: Chapter 1 problem 1.9."""
+
+
+def is_substring(s1, s2):
+    """Check if s2 is a substring of s1."""
+    val = s1.find(s2)
+    if val == -1:
+        return False
+    else:
+        return True
+
+
+def is_rotation(s1, s2):
+    """Check if s2 reversed is a substring of s1."""
+    return is_substring(s1, s2[::-1])
+
+if __name__ == '__main__':
+
+    w1 = 'waterbottle'
+    w2 = 'retawf'
+
+    print(is_rotation(w1, w2))
