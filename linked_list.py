b29e3ccd33a3072e08218b0849694ecd6769e1c9
Sacha Gunaratne <sacha.gunaratne@gmail.com>
2018-08-12 23:45:42 -0400

    Completed the kth_last_element linked list problem.

--- linked_list.py
+++ linked_list.py
@@ -18,7 +18,7 @@ class Node():
             if n.next is not None:
                 s += '-->'
             n = n.next
-        
+
         s += str(n.data)
         return s
 
@@ -48,7 +48,7 @@ class Node():
 
 
 def remove_dups(linked_list):
-    """Remove duplicates from the singly linked list. Chapter 2, problem 2.1"""
+    """Remove duplicates from the singly linked list. Chapter 2, problem 2.1."""
     buffer = []
     n = linked_list
     while n.next is not None:
@@ -61,12 +61,36 @@ def remove_dups(linked_list):
             break
     return linked_list
 
-if __name__ == '__main__':
+
+def kth_to_last(linked_list, k, index=[0]):
+    """Find the kth to last element. i.e., if k=1 return the last element, if k=2 return the second to last element.
+
+    The assumption is that the length of the list is unknown.
+    """
+    if linked_list is None:
+        return None
+    
+    node = kth_to_last(linked_list.next, k, index)
+    index[0] += 1 
+    if index[0] == k:
+        return linked_list
+
+    return node
+    
+def test():
     linked_list = Node(1)
     for i in range(5):
         linked_list.append_to_tail(i)
-    
+
     linked_list.append_to_tail(1)
 
     print(linked_list)
     print(remove_dups(linked_list))
+
+if __name__ == '__main__':
+    linked_list = Node(1)
+    linked_list.append_to_tail(2)
+    linked_list.append_to_tail(3)
+
+    print(kth_to_last(linked_list, 2))
+    
